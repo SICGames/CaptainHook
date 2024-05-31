7b07b595ac8779843d8a03ca9a47bb84501d8263
SICGames <paul.skertich@hotmail.com>
2024-05-31 16:50:39 -0400

    Updated Assembly Information for Nuget

--- CaptainHook/Properties/AssemblyInfo.cs
+++ CaptainHook/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CaptainHook")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Mouse and Keyboard Hooking .NET Framework Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SICGames")]
 [assembly: AssemblyProduct("CaptainHook")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
